72573ec51bf5be25c19724e5ca0a1674f798d7ab
monmatprogramer <monsokmeanmon@gmail.com>
2025-08-14 10:25:51 +0700

    remove unused imports in DesignerPage

--- lib/pages/designer_page.dart
+++ lib/pages/designer_page.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:presence_app/pages/morning_list_page.dart';
 import 'package:presence_app/pages/segmented_controll_app.dart';
-import 'package:presence_app/pages/tap_bar_app.dart';
 
 class DesignerPage extends StatelessWidget {
   const DesignerPage({super.key});
