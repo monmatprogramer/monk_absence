4694dc2df3d02bffd41786fc1dcd3c3ad3f8b8cd
monmatprogramer <monsokmeanmon@gmail.com>
2025-09-24 20:28:30 +0700

    change

--- lib/services/api_service_dio.dart
+++ lib/services/api_service_dio.dart
@@ -1,6 +1,17 @@
 
 import 'package:dio/dio.dart';
+import 'package:presence_app/db_config.dart';
 
 class ApiServerDio{
-    final Dio dio = Dio(BaseOptions());
+    final Dio dio = Dio(BaseOptions(
+        baseUrl: "${DbConfig.apiUrl}/api",
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 15),
+        headers: {"Content-Type": "application/json"},
+    ));
+
+    // Upload image from Web app
+    Future<void> uploadImage(String token, List<int> bytes, String filename)async{
+
+    }
 }
